c9cf42ef862cdb8f0106a2cfa60b4615fa5ebc6a
jos_aa <jos_aaron@hotmail.com>
2021-07-20 17:54:45 -0600

    adding inquire

---
+++ app.js
@@ -0,0 +1,18 @@
+const { mostrarMenu, pausa } = require('./helpers/mensajes')
+
+require('colors')
+console.clear()
+const main = async() =>{
+  
+    let opt = ' '
+   
+    do{
+        
+        opt = await mostrarMenu()
+        if(opt !== '0')  await pausa()
+
+    }while(opt !== '0')    
+
+}
+
+main()
